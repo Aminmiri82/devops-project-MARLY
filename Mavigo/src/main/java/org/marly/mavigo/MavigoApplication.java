bacc72bd86d1a26f49e124632c66becd92b6ce65
YM82 <miri.amin@ymail.com>
2025-10-23 21:29:34 +0200

    added boot spring

---
+++ Mavigo/src/main/java/org/marly/mavigo/MavigoApplication.java
@@ -0,0 +1,13 @@
+package org.marly.mavigo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MavigoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MavigoApplication.class, args);
+	}
+
+}
---
+++ Mavigo/src/test/java/org/marly/mavigo/MavigoApplicationTests.java
@@ -0,0 +1,13 @@
+package org.marly.mavigo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MavigoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
