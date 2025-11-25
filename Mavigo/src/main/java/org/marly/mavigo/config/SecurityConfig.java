18485f310b0d7f42d62f05a899186bb04590cd75
Raphael Meimoun <raphael.meimoun95@gmail.com>
2025-11-25 14:12:01 +0100

    ajout de l'api google task

---
+++ Mavigo/src/main/java/org/marly/mavigo/config/SecurityConfig.java
@@ -0,0 +1,29 @@
+package org.marly.mavigo.config;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/actuator/**", "/h2-console/**", "/").permitAll()
+                        .requestMatchers("/api/google/**").authenticated()
+                        .anyRequest().permitAll()
+                )
+                .oauth2Login(o -> o.defaultSuccessUrl("/api/google/tasks/me", true))
+                .oauth2Client(withDefaults())
+                .headers(h -> h.frameOptions(frame -> frame.sameOrigin()))
+                .csrf(csrf -> csrf.disable());
+        return http.build();
+    }
+}
