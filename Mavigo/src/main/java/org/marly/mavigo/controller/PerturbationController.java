2f6d0be0487e6159dd9ae13928fe1b24937a95ab
YM82 <miri.amin@ymail.com>
2025-12-13 18:34:09 +0100

    refactor: remove disruption tester from frontend

--- Mavigo/src/main/java/org/marly/mavigo/controller/PerturbationController.java
+++ Mavigo/src/main/java/org/marly/mavigo/controller/PerturbationController.java
@@ -1,10 +1,17 @@
 package org.marly.mavigo.controller;
 
 import java.util.List;
+
 import org.marly.mavigo.models.disruption.Disruption;
 import org.marly.mavigo.service.perturbation.PerturbationService;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/perturbations")
