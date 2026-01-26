1f728b019d8a68247a1736714b4462b266a62e3f
maladoooo <maladocoumbassy@gmail.com>
2026-01-26 22:37:43 +0100

    fix: handle duplicate stop areas in database queries
    Use findFirstByNameIgnoreCase instead of findByNameIgnoreCase to
    handle cases where multiple stop areas have the same name. The previous
    query would fail with "Query did not return a unique result" when
    there were duplicates.

--- Mavigo/src/main/java/org/marly/mavigo/repository/StopAreaRepository.java
+++ Mavigo/src/main/java/org/marly/mavigo/repository/StopAreaRepository.java
@@ -1,5 +1,6 @@
 package org.marly.mavigo.repository;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -9,7 +10,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface StopAreaRepository extends JpaRepository<StopArea, UUID> {
 
     Optional<StopArea> findByExternalId(String externalId);
-    
-    Optional<StopArea> findByNameIgnoreCase(String name);
+
+    List<StopArea> findByNameIgnoreCase(String name);
+
+    Optional<StopArea> findFirstByNameIgnoreCase(String name);
 }
 
