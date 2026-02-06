739cbb2ec488a3a8d80a4b94c5ac6d9101738eeb
Raphael Meimoun <raphael.meimoun95@gmail.com>
2026-02-06 15:17:00 +0100

    fix: creation de task en local  supp

--- Mavigo/src/main/resources/static/js/core/state.js
+++ Mavigo/src/main/resources/static/js/core/state.js
@@ -3,6 +3,8 @@ export const state = {
   currentView: localStorage.getItem("mavigo_view") || "journey",
   defaultTaskList: null,
   currentJourney: null,
+  /** Derniers itinéraires affichés (avec includedTasks), pour les conserver au start. */
+  lastDisplayedJourneys: null,
   lastNotifiedJourneyId: null,
   lastTasksSignature: null,
   pendingSuggestionTask: null,
